293ae4b27f2dc68023d20d235b9ad67b70c7249d
Somsubhra Das <das.somsubhra1@gmail.com>
2022-02-16 10:28:35 +0530

    intial commit and boiler plate setup

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import { createConnection } from "typeorm";
+import { resolvers } from "./resolvers/";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageProductionDefault,
+} from "apollo-server-core";
+
+async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers,
+  });
+
+  const app = express();
+
+  const server = new ApolloServer({
+    schema,
+    context: ({ req, res }) => ({ req, res }),
+    plugins: [
+      process.env.NODE_ENV === "production"
+        ? ApolloServerPluginLandingPageProductionDefault
+        : ApolloServerPluginLandingPageGraphQLPlayground,
+    ],
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app, cors: false });
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`App is running on PORT ${PORT}`);
+  });
+
+  // connect to db
+
+  await createConnection();
+}
+
+bootstrap();
